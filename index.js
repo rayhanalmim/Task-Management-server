ea57a6f57643c7a8ee905adcb9d61e219c3d067d
rayhanalmim <rayhanalmim1@gmail.com>
2023-12-21 20:59:16 +0600

    task add api endpoint implemented

--- index.js
+++ index.js
@@ -20,10 +20,75 @@ db.on('error', (err) => {
   console.error(`Error connecting to MongoDB: ${err}`);
 });
 
+const taskData = {
+    tasks: [
+    ],
+    columns: {
+        "column-1": {
+            id: "column-1",
+            title: "TO-DO",
+            taskIds: [],
+        },
+        "column-2": {
+            id: "column-2",
+            title: "IN-PROGRESS",
+            taskIds: [],
+        },
+        "column-3": {
+            id: "column-3",
+            title: "COMPLETED",
+            taskIds: [],
+        },
+    },
+    columnOrder: ["column-1", "column-2", "column-3"],
+};
+
 db.once('open', () => {
   console.log('Connected to MongoDB');
 
   const userwisetask = mongoose.model('task', new mongoose.Schema({}, { strict: false }));
+
+  app.post('/createtaskschema', async (req, res) =>{
+    const email = req.query.email;
+    const result = await userwisetask.find({ email: email })
+    if(result.length){
+        return res.send({message: 'user schema already exists'});
+    }
+    if(email === 'undefined'){
+        return res.send({message: 'no valid user'});
+    }
+    const newSchema = {...taskData, email: email }
+    console.log(newSchema)
+    const insert = await userwisetask.create(newSchema);
+    res.send(insert);
+  });
+
+  app.get('/taskdata', async(req, res)=>{
+    const email = req.query.email;
+    const result = await userwisetask.find({ email: email })
+    res.send(result)
+  })
+
+  app.post('/createTask', async (req, res)=>{
+    const data = req.body;
+    const email = req.query.user;
+    console.log(data, email)
+    const task = await userwisetask.find({ email: email })
+    console.log(task)
+    const createId = task[0].tasks.length;
+    console.log(createId)
+
+    const newData = {...data, id: createId}
+
+    const update = await userwisetask.updateOne(
+      { email: email },
+      {
+        $push: { tasks: newData, 'columns.column-1.taskIds': createId },
+      },
+    );
+
+    res.send(update)
+  })
 
 });
 
